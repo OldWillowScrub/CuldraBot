79906787c2ad0b7f5ec41e3e5d13e7a19ba5b96b
OldWillowScrub <38382586+OldWillowScrub@users.noreply.github.com>
2018-04-14 15:39:40 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,12 +7,12 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content.startsWith(prefix + "Card") {
-       message.channel.send("Revolt Card Database, type ?Name of Card Here to get the stats of any card you want, e.g., ?bandit \n\nDatabase Is 100% Complete. All Creatures, Items, and Spells have been added! For random book help, type ?Book");
-    }
-    if (message.content.startsWith(prefix + "card") {
-         message.channel.send("Revolt Card Database, type ?Name of Card Here to get the stats of any card you want, e.g., ?bandit \n\nDatabase Is 100% Complete. All Creatures, Items, and Spells have been added! For random book help, type ?Book");
-    }
+  if (message.content.startsWith(prefix + "Card")) {
+    message.channel.send("Revolt Card Database, type ?Name of Card Here to get the stats of any card you want, e.g., ?bandit \n\nDatabase Is 100% Complete. All Creatures, Items, and Spells have been added! For random book help, type ?Book");
+  }
+  if (message.content.startsWith(prefix + "card")) {
+    message.channel.send("Revolt Card Database, type ?Name of Card Here to get the stats of any card you want, e.g., ?bandit \n\nDatabase Is 100% Complete. All Creatures, Items, and Spells have been added! For random book help, type ?Book");
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
