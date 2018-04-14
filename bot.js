4a97b0e8d971abe64919867971213c8387d2e886
OldWillowScrub <38382586+OldWillowScrub@users.noreply.github.com>
2018-04-14 15:03:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', message => {
   if (message.content.startsWith(prefix + "Card") {
    message.channel.send('Revolt Card Database, type ?Name of Card Here to get the stats of any card you want, e.g., ?bandit \n\nDatabase Is 100% Complete. All Creatures, Items, and Spells have been added!');
 }
-if (message.content.startsWith(prefix + "card") {
+  if (message.content.startsWith(prefix + "card") {
    message.channel.send('Revolt Card Database, type ?Name of Card Here to get the stats of any card you want, e.g., ?bandit \n\nDatabase Is 100% Complete. All Creatures, Items, and Spells have been added!');
 }
 });
