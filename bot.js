eddadd285a35f6934cb3e9f2016547ebf93631ee
OldWillowScrub <38382586+OldWillowScrub@users.noreply.github.com>
2018-04-14 13:56:27 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const fs = require("fs");
-const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+const configJSON	= require("./Configurations/config.json");
 
 client.on('ready', () => {
     console.log('I am ready!');
