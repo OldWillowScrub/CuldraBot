d38a66dcc994aa2655939ca70c13525730c540f4
OldWillowScrub <38382586+OldWillowScrub@users.noreply.github.com>
2018-04-14 14:26:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,12 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
-  	}
+  } else
+  if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+  if(message.content.startsWith(prefix + "book") {
+    message.channel.send('To let the bot pick a random 20/10/20 Book for you, filled with 50 different cards, type ?randombook \n\nWant your Revolt Book in one png file? type ?book2png');
 });
 
 // THIS  MUST  BE  THIS  WAY
