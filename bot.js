38414b1d5483ea8ad2788faeb05656c16f8f175e
OldWillowScrub <38382586+OldWillowScrub@users.noreply.github.com>
2018-04-14 13:41:14 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,10 @@ client.on('message', message => {
   	}
 });
 
+  //BOOK HELP GUIDE
+    if(msg.content === config.prefix + "Book") {
+         msg.channel.send('To let the bot pick a random 20/10/20 Book for you, filled with 50 different cards, type ?RandomBook \n\nWant your Revolt Book in one png file? type ?book2png');
+    }
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
