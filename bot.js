0b049fe4d7ae2e2b4d14bc0fa1864ae61223d226
OldWillowScrub <38382586+OldWillowScrub@users.noreply.github.com>
2018-04-14 13:43:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,15 +6,10 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-  //BOOK HELP GUIDE
     if(msg.content === config.prefix + "Book") {
          msg.channel.send('To let the bot pick a random 20/10/20 Book for you, filled with 50 different cards, type ?RandomBook \n\nWant your Revolt Book in one png file? type ?book2png');
-    }
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
