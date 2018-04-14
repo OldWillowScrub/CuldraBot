ff76a15246afef2196331ba3b19668f0b1757848
OldWillowScrub <38382586+OldWillowScrub@users.noreply.github.com>
2018-04-14 13:36:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,24 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = "?";
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+const fs = require("fs");
+const config = JSON.parse(fs.readFileSync(".config.json", "utf8"));
 
-//BOOK HELP GUIDE
-    if(msg.content === prefix + "Book") {
+bot.on("ready", function() {
+  console.log(`Read as: ${bot.user.tag}`)
+})
+
+bot.on("message", function(msg){
+  if(msg.author.bot) {
+    return;
+  }
+
+    bot.user.setGame(' Type ?Card for help')
+
+  //BOOK HELP GUIDE
+    if(msg.content === config.prefix + "Book") {
          msg.channel.send('To let the bot pick a random 20/10/20 Book for you, filled with 50 different cards, type ?RandomBook \n\nWant your Revolt Book in one png file? type ?book2png');
+    }
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
