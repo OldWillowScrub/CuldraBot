e1168d33251a7e638eb08c57372deb58d25af03d
OldWillowScrub <38382586+OldWillowScrub@users.noreply.github.com>
2018-04-14 13:17:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,14 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "?";
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+//BOOK HELP GUIDE
+    if(msg.content === config.prefix + "Book") {
+         msg.channel.send('To let the bot pick a random 20/10/20 Book for you, filled with 50 different cards, type ?RandomBook \n\nWant your Revolt Book in one png file? type ?book2png');
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
