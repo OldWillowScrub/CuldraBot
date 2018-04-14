7ae5fa7fe9fbe7fa9d687d57a08396f4b088c1aa
OldWillowScrub <38382586+OldWillowScrub@users.noreply.github.com>
2018-04-14 13:31:43 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 });
 
 //BOOK HELP GUIDE
-    if(msg.content === config.prefix + "Book") {
+    if(msg.content === prefix + "Book") {
          msg.channel.send('To let the bot pick a random 20/10/20 Book for you, filled with 50 different cards, type ?RandomBook \n\nWant your Revolt Book in one png file? type ?book2png');
 
 // THIS  MUST  BE  THIS  WAY
