38ed87182cdf6adc37e93091571a787e003e8cba
OldWillowScrub <38382586+OldWillowScrub@users.noreply.github.com>
2018-04-16 00:07:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "?";
 
-{game:
-  {name:"you sleep O_O",
-    type:3
-  }
-}
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
