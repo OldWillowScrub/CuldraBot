e36b23e04957f806a0bafa6bf8ba7fd31ab78e0f
OldWillowScrub <38382586+OldWillowScrub@users.noreply.github.com>
2018-04-15 23:26:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
   if (message.content.startsWith(prefix + "card")) {
     message.channel.send("Revolt Card Database, type ?Name of Card Here to get the stats of any card you want, e.g., ?bandit \n\nDatabase Is 100% Complete. All Creatures, Items, and Spells have been added! For random book help, type ?Book");
   }
-  bot.user.setGame(' Type ?Card for help')  
+  bot.user.setPresence({ game: { name: 'Type ?Card for help', type: 0 } }); 
   //Neutral Creatures Go Here  
   if (message.content.startsWith(prefix + "Androgear")) {
     message.channel.send("Androgear <:craritye:364048663611310081> <:cneutral:364043997825597440> \nST:60 HP:60 MHP:60 G:250+ <:ccard:364048663426760705> \nSupport: <:cneutral:364043997825597440> / Attacks First / Transforms into Gearion if Beast Gear is used as support / Neutralizes: <:cscroll:364048663691001876> Attacks", {files: ["http://www.culdceptcentral.com/images/cards_sm/cr3ds/neutral/androgear.jpg"]});
